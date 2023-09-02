eb61c4861cb14b9cf6d59c1a9207de3b665e94c5
shhyunn <limgm7@gmail.com>
2023-09-02 16:55:57 +0900

    apply secrets

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import openai 
 import streamlit as st
 import os
-# from dotenv import load_dotenv
 from textblob_text import TextBlob
 # import random
 
@@ -18,13 +17,13 @@ def grade_emotion(sentiment):
 # load_dotenv()
 st.set_page_config(page_title="Emotion Analysis Chatbot", page_icon="😡")
 st.title("💬 Chatbot") 
-# openai_api_key = os.getenv('OPENAI_API_KEY')
+openai_api_key = st.secrets["OPENAI_API_KEY"]
 
-with st.sidebar:
-    openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
-    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
-    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
-    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
+# with st.sidebar:
+#     openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
+#     "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
+#     "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
+#     "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
 
 if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
